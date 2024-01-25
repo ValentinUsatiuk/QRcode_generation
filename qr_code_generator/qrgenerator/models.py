c006160fa84f68a241c4bab31ed9659b5f160435
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2024-01-25 11:28:24 +0200

    Add data settings in models and make migrations

--- qr_code_generator/qrgenerator/models.py
+++ qr_code_generator/qrgenerator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class QRCodeInfo(models.Model):
+    data = models.TextField()
+    color = models.CharField(max_length=20, default="black")
+    size = models.PositiveIntegerField(default=20)
