4bb4f69f3100f86feedc8d3ba81aebe2b886a50f
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2024-01-25 11:48:37 +0200

    Fix bug

--- qr_code_generator/qr_code_generator/urls.py
+++ qr_code_generator/qr_code_generator/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('qrgenerator/', include('qrgenerator.urls'))
+    path('', include('qrgenerator.urls'))
 ]
