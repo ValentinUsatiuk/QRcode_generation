102639d346b922fe482aa4e2140dc92cafe48277
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2024-01-25 11:39:46 +0200

    Fix bug

--- qr_code_generator/qrgenerator/urls.py
+++ qr_code_generator/qrgenerator/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import generate_qr_code, display_qr_code
+from .views import generate_qr_code, display_qr_codes
 
 urlpatterns = [
     path("generate/", generate_qr_code, name="generate_qr_code"),
-    path("display/", display_qr_code, name="display_qr_code"),
+    path("display/", display_qr_codes, name="display_qr_code"),
 ]
